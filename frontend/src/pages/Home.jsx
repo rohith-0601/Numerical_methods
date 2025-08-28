c8069aef9242f05bf74e8a42111a8f6a7b8f180c
rohith-0601 <rohithperugu3@gmail.com>
2025-08-28 21:24:31 +0530

    working on pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,44 +1,72 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
 
 function HomePage() {
-    return ( 
+  return (
     <div className="container">
-      <h1 className="heading">Don't take our word for it</h1>
+      <h1 className="heading">Prime Numbers Assignment </h1>
       <div className="cardGrid">
-
         {/* Card 1 */}
-        <div className="card orangeCard">
-          <div className="cardContent">
-            <p className="quote">"This proactive support helped us overcome initial deployment challenges and optimize our use of the platform."</p>
-            <p className="author">Abubakar Mohd</p>
-            <p className="role">Chief Technology Officer, Beyon Cyber</p>
+        <div className="card orangeCard q1Card">
+          <div className="cardContent q1Content">
+            <h3 className="q1Title">Question 1</h3>
+            <p className="quote q1Text">
+              A prime number is <b>12345678910987654321</b>. Here n is 10.
+              <br />
+              Find the next number that follows this pattern.
+              <br />
+              That number n lies between 1000 and 3000.
+              <br />
+              This was discovered by an <b>Indian</b>.
+            </p>
+            <button className="q1Button">Go to Q1</button>
           </div>
         </div>
 
         {/* Card 2 */}
-        <div className="card greenCard">
-          <div className="cardHeader">
-            <span className="caseStudyTag">CASE STUDIES</span>
-            <span className="readMore">Read now →</span>
-          </div>
-          <p className="cardText">Beyon Cyber uses Tines to fuel expansion and innovation</p>
-          <div className="logoPlaceholder">
-            <img src="https://i.imgur.com/e2n5q3o.png" alt="Beyon Cyber Logo" className="logo" />
-          </div>
-        </div>
+       <div className="card q2Card">
+  <div className="cardContent q2Content">
+    <h3 className="q2Title">Question 2</h3>
+    <p className="q2Text">
+      11 is prime, 111 is not prime. We use the notation, 1<sub>N</sub> means N ones.
+      For example, for N = 7, we mean seven ones: 1111111.  
+      1<sub>N</sub> is represented by (10<sup>N−1</sup> − 1)/9.
+    </p>
+    <p className="q2Text">
+      If N is prime, 1<sub>N</sub> might be prime. If N is not prime, 1<sub>N</sub> cannot be prime. 
+      Thus, we have to check only when N is prime.
+    </p>
+    <p className="q2Text">
+      Your task is to determine the 5 primes between N = 2 and N = 1040. 
+      Remember that this involves both <strong>prime checking</strong> and 
+      <strong> pattern recognition</strong>.
+    </p>
+    <button className="q2Button" onClick={() => window.location.href='/q2'}>
+      Go to Q2
+    </button>
+  </div>
+</div>
+
+
 
         {/* Card 3 */}
-        <div className="card pinkCard">
-          <div className="cardHeader">
-            <span className="readMore">Read now →</span>
-          </div>
-          <p className="cardText">"Tines' service has made a huge impact on our decision-making with the product and internally."</p>
-          <div className="authorInfo">
-            <p className="author">Security Leader</p>
-            <p className="role">National Research Firm</p>
-          </div>
-        </div>
+       {/* Card 3 */}
+<div className="card q3Card">
+  <div className="q3Header">Question 3</div>
+  <p className="q3Text">
+    We are interested in Mersenne primes. A Mersenne prime is a prime number 
+    that is one less than a power of two. The largest Mersenne prime discovered 
+    was on Oct 12, 2024 when 2^(p-1) where p=136,279,841. This has 41,024,320 digits. 
+    Find the two primes where p lies between 2201 and 2299. 
+    These primes were discovered in 1952.
+  </p>
+  <div className="q3ButtonWrapper">
+    <button className="q3Button" onClick={() => window.location.href='/q3'}>
+      Go to Q3 
+    </button>
+  </div>
+</div>
+
 
         {/* Card 4 */}
         <div className="card purpleCard">
@@ -46,9 +74,16 @@ function HomePage() {
             <span className="caseStudyTag">CASE STUDIES</span>
             <span className="readMore">Read now →</span>
           </div>
-          <p className="cardText">Mars achieves coverage of 80-90% of sources for true positives in weeks</p>
+          <p className="cardText">
+            Mars achieves coverage of 80-90% of sources for true positives in
+            weeks
+          </p>
           <div className="logoPlaceholder">
-            <img src="https://i.imgur.com/w1iV18V.png" alt="MARS Logo" className="logo" />
+            <img
+              src="https://i.imgur.com/w1iV18V.png"
+              alt="MARS Logo"
+              className="logo"
+            />
           </div>
         </div>
 
@@ -57,10 +92,16 @@ function HomePage() {
           <div className="cardHeader">
             <span className="readMore">Read now →</span>
           </div>
-          <p className="cardText">"[With Tines professional services]we were able to build whatever we wanted, and even add more features that we didn't previously cater for."</p>
+          <p className="cardText">
+            "[With Tines professional services]we were able to build whatever we
+            wanted, and even add more features that we didn't previously cater
+            for."
+          </p>
           <div className="authorInfo">
             <p className="author">Issa Almannaei</p>
-            <p className="role">Head of Security Operations Center, Beyon Cyber</p>
+            <p className="role">
+              Head of Security Operations Center, Beyon Cyber
+            </p>
           </div>
         </div>
 
@@ -73,12 +114,13 @@ function HomePage() {
         {/* Card 7 */}
         <div className="card beigeCard">
           <div className="largeNumber">2x</div>
-          <p className="cardText">faster product adoption with professional services</p>
+          <p className="cardText">
+            faster product adoption with professional services
+          </p>
         </div>
       </div>
     </div>
   );
-
 }
 
 export default HomePage;
